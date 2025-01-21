8710f4c6fec03fcad7be2bbb3db3a4f51732837d
mlwelker <nutria.watts.0g@icloud.com>
2025-01-21 01:17:45 -0600

    using a Constants struct

--- warm-up/ContentView.swift
+++ warm-up/ContentView.swift
@@ -4,34 +4,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(content: {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-                .background(Color.gray.opacity(0.2))
+                .background(Constants.bgColor)
             Text("Hello, world!")
-                .background(Color.gray.opacity(0.2))
+                .background(Constants.bgColor)
             HStack {
                 ShapeView()
                 ShapeView()
             }
-        }
-        .background(Color.gray.opacity(0.2))
+        })
+        .background(Constants.bgColor)
         .padding()
     }
 }
 
+struct Constants {
+    static let aspectRatio: CGFloat = 2/3
+    static let bgColor: Color = .gray.opacity(0.2)
+}
+
 struct ShapeView: View {
     var body: some View {
-        GeometryReader { geometry in
-            let xOrigin = geometry.size.width / 2
-            let yOrigin = geometry.size.height / 2
-            
-            RoundedRectangle(cornerRadius: 12)
-                .aspectRatio(2/3, contentMode: .fit)
-//                .stroke(.blue, style: StrokeStyle(lineWidth: 12, lineCap: .round))
-                .background(Color.gray.opacity(0.2))
-        }
+        RoundedRectangle(cornerRadius: 12)
+            .stroke(.blue.secondary, style: StrokeStyle(lineWidth: 6))
+            .aspectRatio(Constants.aspectRatio, contentMode: .fit)
+            .background(Constants.bgColor)
+            .padding()
     }
 }
 
