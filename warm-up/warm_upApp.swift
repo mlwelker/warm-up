daa9c1a712c1082009ffbc0c56eab21d9c048c63
mlwelker <nutria.watts.0g@icloud.com>
2025-01-20 18:26:21 -0600

    Initial Commit

---
+++ warm-up/warm_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  warm_upApp.swift
+//  warm-up
+//
+//  Created by Michael Welker on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct warm_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
