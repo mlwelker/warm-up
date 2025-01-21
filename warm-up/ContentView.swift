ab2079d147e9f39301e589e33b50fb60515832cb
mlwelker <nutria.watts.0g@icloud.com>
2025-01-21 09:15:11 -0600

    a groovy MeshGradient

--- warm-up/ContentView.swift
+++ warm-up/ContentView.swift
@@ -4,35 +4,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(content: {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-                .background(Constants.bgColor)
-            Text("Hello, world!")
-                .background(Constants.bgColor)
-            HStack {
-                ShapeView()
-                ShapeView()
-            }
-        })
-        .background(Constants.bgColor)
-        .padding()
-    }
-}
-
-struct Constants {
-    static let aspectRatio: CGFloat = 2/3
-    static let bgColor: Color = .gray.opacity(0.2)
-}
-
-struct ShapeView: View {
-    var body: some View {
-        RoundedRectangle(cornerRadius: 12)
-            .stroke(.blue.secondary, style: StrokeStyle(lineWidth: 6))
-            .aspectRatio(Constants.aspectRatio, contentMode: .fit)
-            .background(Constants.bgColor)
-            .padding()
+        GeometryReader { geometry in
+            MeshGradient(
+                width: 5,
+                height: 3,
+                points: [
+                    [0.0, 0.0], [0.25, 0.0], [0.5, 0.0], [0.75, 0.0], [1.0, 0.0],
+                    [0.0, 0.5], [0.25, 0.25], [0.5, 0.5], [0.75, 0.75], [1.0, 0.75],
+                    [0.0, 1.0], [0.25, 1.0], [0.5, 1.0], [0.75, 1.0], [1.0, 1.0]
+                ],
+                colors: [
+                    .blue, .blue, .cyan, .mint, .green,
+                    .yellow, .orange, .orange, .orange, .yellow,
+                    .indigo, .purple, .pink, .pink, .red
+                ]
+            )
+        }
+        .ignoresSafeArea()
     }
 }
 
