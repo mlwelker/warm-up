8d243d11283c73f2be1ec1e07bdce57a0828eeff
mlwelker <nutria.watts.0g@icloud.com>
2025-01-21 19:02:23 -0600

    RatingView (with + and - buttons)

--- warm-up/ContentView.swift
+++ warm-up/ContentView.swift
@@ -3,26 +3,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showIcon: Bool = true
-    @State private var showBackground: Bool = true
-    
     var body: some View {
         ZStack {
             GroovyBackground()
             VStack {
                 Spacer()
-                if showIcon {
-                    UserImage()
-                        .transition(.scale.combined(with: .opacity))
-                }
+                RatingView()
                 Spacer()
-                Button("Show/Hide") {
-                    withAnimation {
-                        showIcon.toggle()
-                    }
-                }
-                .buttonStyle(.borderedProminent)
             }
+        }
+    }
+}
+
+struct UserHideShowView: View {
+    @State private var showIcon: Bool = true
+    
+    var body: some View {
+        if showIcon {
+            UserImage()
+                .transition(.scale.combined(with: .opacity))
+        }
+        Spacer()
+        Button("Show/Hide") {
+            withAnimation {
+                showIcon.toggle()
+            }
+        }
+        .buttonStyle(.borderedProminent)
+    }
+}
+
+struct RatingView: View {
+    @State private var rating: Int = 5
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.gray.secondary)
+                .aspectRatio(5/3, contentMode: .fit)
+                .padding(20)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.white)
+                .aspectRatio(5/3, contentMode: .fit)
+                .padding(40)
+            
+            HStack {
+                Button {
+                    rating -= 1
+                } label: {
+                    Image(systemName: "minus.circle")
+                }.disabled(rating == 0)
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Text(String(format: "%02d", rating))
+                    .font(Font.system(size: 80, weight: .bold))
+                
+                Spacer()
+                
+                Button {
+                    rating += 1
+                } label: {
+                    Image(systemName: "plus.circle")
+                }.disabled(rating >= 10)
+                    .font(.largeTitle)
+            }
+            .padding(.horizontal, 80)
         }
     }
 }
@@ -32,7 +80,7 @@ struct UserImage: View {
         Image(systemName: "person.circle.fill")
             .resizable()
             .scaledToFit()
-            .padding()
+            .padding(50)
             .foregroundStyle(.white)
     }
 }
